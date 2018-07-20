a73021bb9228f4a840a967ea8db0ce98d4c6572e
Target Software <targetqa@targetsoftware.com.br>
2018-07-20 12:32:34 -0300

    commit

--- QAPortalCorporativo/src/test/java/qa/target/portalcorporativo/core/DriverFactory.java
+++ QAPortalCorporativo/src/test/java/qa/target/portalcorporativo/core/DriverFactory.java
@@ -1,5 +1,29 @@
 package qa.target.portalcorporativo.core;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class DriverFactory {
+	
+	private static WebDriver driver;
+
+    public static WebDriver GetDriver()
+    {
+        if (driver == null)
+            driver = new ChromeDriver();          
+
+        return driver;
+    }
+
+    
+    public static void KillDriver()
+    {
+        if (driver != null)
+        {
+            driver.quit();
+            driver = null;
+
+        }
+    }
 
 }
