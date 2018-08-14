d7d424e2ebb4a9ec498105784eae2f03e5b48139
carolinasampognaTgT <carolina.sampogna@targetsoftware.com.br>
2018-08-14 16:25:56 -0300

    Page departamento
    Page departamento

--- src/test/java/qa/portalcorporativo/tests/MenuGerencialDepartamentoTest.java
+++ src/test/java/qa/portalcorporativo/tests/MenuGerencialDepartamentoTest.java
@@ -1,6 +1,30 @@
 package qa.portalcorporativo.tests;
 
-public class MenuGerencialDepartamentoTest {
+
+import qa.portalcorporativo.page.MenuGerencialDepartamentoPage;
+import org.junit.Test;
+import qa.portalcorporativo.core.BaseTest;
+
+public class MenuGerencialDepartamentoTest  extends BaseTest{
+	
+	
+MenuGerencialDepartamentoPage page = new MenuGerencialDepartamentoPage();
+	
+	@Test
+	
+	public void CadastrarNovoDepartamento() throws InterruptedException {
+		
+		page.SetClicarMenuGerencial();
+		page.SetClicarDepartamento();
+		page.SetClicarNovoItem();
+		page.SetClicarNome();
+		page.SetClicarDescrição();
+		page.SetClicarEmail();
+		page.SetClicarAtivo();
+		page.SetSalvar();
+		
+	}
+	
 
 	
 
